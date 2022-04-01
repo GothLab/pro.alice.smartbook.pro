4fc322f5026daf569d1ac91e0ade9cf04edbcd0a
blacklovefly <OMLET32@YA.RU>
2022-04-01 17:55:15 +0300

    asd

---
+++ assets/js/BTNS.js
@@ -0,0 +1,65 @@
+
+
+var TXT = 'ORIG'
+var EPR = -1;
+var TXT1 = 'BZAH';
+var TXT2 = 'NDEM';
+var TXT3 = 'VNAB';
+
+
+function loadNext() {  
+
+EPR = EPR + 1; 
+
+swiper2.removeAllSlides();
+
+swiper2.appendSlide('<div class="swiper-slide" id="M-1"><>');
+        
+$('#M-1').load('./res/' + TXT + '.html #A-' + EPR ,function(){ 
+
+swiper2.update(); 
+
+toastInit();   
+
+$('.TR').tooltip('enable');
+        
+ $('#T-1').load('./res/' + TXT1 + '.html #A-' + EPR);
+
+$('#T-2').load('./res/' + TXT2 + '.html #A-' + EPR);
+
+$('#T-3').load('./res/' + TXT3 + '.html #A-' + EPR, function(){swiper3.update();});
+
+});      
+
+}
+
+
+
+
+function loadPrev() {  
+
+EPR = EPR - 1;    
+
+swiper2.removeAllSlides();
+        
+swiper2.appendSlide('<div class="swiper-slide" id="M-1"><>');
+        
+$('#M-1').load('./res/' + TXT + '.html #A-' + EPR ,function(){ 
+        
+toastInit();
+
+swiper2.update();  
+
+$('.TR').tooltip('enable');
+        
+$('#T-1').load('./res/' + TXT1 + '.html #A-' + EPR);
+
+$('#T-2').load('./res/' + TXT2 + '.html #A-' + EPR);
+
+$('#T-3').load('./res/' + TXT3 + '.html #A-' + EPR, function(){swiper3.update();} );
+
+});
+      
+        
+}
+   loadNext();
